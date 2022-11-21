6f1355a7db01b7971a4ff74f0fac2458adc389dd
libel07deceits <118745726+libel07deceits@users.noreply.github.com>
2022-11-21 22:16:10 +0800

    Create dd.py

---
+++ dd.py
@@ -0,0 +1,11 @@
+ def unique(l):
+      if len(l)==len(set(l)):
+          print("All elements are unique")
+      else:
+          print("List has duplicates")
+
+  unique([1,2,3,4])
+  # All elements are unique
+  
+  unique([1,1,2,3])
+  # List has duplicates
